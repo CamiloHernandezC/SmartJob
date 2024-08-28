bef66de7bf129ff0a3557daa4518d5cbee911485
Camilo Hernandez <camehernandecas@unal.edu.co>
2024-08-27 19:05:46 -0500

    Service Registry

---
+++ ServiceRegister/src/test/java/com/fetcher/service/register/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetcher.service.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
